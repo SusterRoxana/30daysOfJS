da23407bb86e9061c2cefea0ef1851e735850dc7
rsuster <roxana.suster@anaplan.com>
2023-05-06 20:17:55 +0300

    day1

---
+++ day1/datatypes.js
@@ -0,0 +1,4 @@
+console.log(typeof phrase)
+console.log(typeof bool)
+console.log(typeof not_known)
+console.log(typeof empty_value)
---
+++ day1/variable.js
@@ -0,0 +1,4 @@
+let phrase = 'There is a string'
+let bool = true
+let not_known
+let empty_value = null
